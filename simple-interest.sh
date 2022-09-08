b060c7d00d0cf6922745cda0e04b944265c75d10
tswede <112585892+tswede@users.noreply.github.com>
2022-09-08 13:08:51 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,7 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # tswede
 
    # Input:
    # p, principal amount
